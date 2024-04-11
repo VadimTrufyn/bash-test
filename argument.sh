29f3ab77961ee09e38a1c8c7f7dfd3dcf7bdc24a
VadimTrufyn <zaq.vadim@gmail.com>
2024-04-11 22:02:22 +0300

    complete script

--- argument.sh
+++ argument.sh
@@ -1,15 +1,62 @@
-!#/bin/bash
+#!/bin/bash
+
 show_help(){
-    echo "use $0 -h -v -b" 
+    echo "Use $0 -d for base name directories 
+    -c numbers of directories
+    -p permissions
+    -f base name of files in each directories
+    -fc numbers of files" 
 }
 
-while getopts "h" opt; do
+creating_files(){
+    for i in $(seq 1 $c); do
+        dir_name="$d${i}"
+        if [ -d "$dir_name"];then
+            echo "such dir exist already"
+            show_help
+            exit 0 
+        else
+            mkdir "$dir_name"
+            chmod 700 "$dir_name"
+            for x in $(seq 1 $n); do
+                file_name="$dir_name/$f$x"
+                touch "$file_name"
+                chmod "$p" "$file_name"
+            done
+        fi
+    done
+}
+
+#default values
+d="my-dir"
+c=1
+p=700
+f="file"
+n=1
+
+#parse arguments using getopts and rewriting values
+while getopts "hd:c:p:f:fc:" opt; do
     case ${opt} in
         h )
             show_help
-            echo "$OPTARG
             exit 0
         ;;
-
+        d )
+            d="$OPTARG"
+        ;;
+        c )
+            c="$OPTARG"
+        ;;
+        p )
+            p="$OPTARG"
+        ;;
+        f )
+            f="$OPTARG"
+        ;;
+        n )
+            n="$OPTARG"
+        ;;
     esac
 done 
+
+creating_files
