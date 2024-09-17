8b1074372bb399c52eac428acf882f69811099fc
Vadym Trufyn <148262369+VadimTrufyn@users.noreply.github.com>
2024-09-17 15:29:19 +0300

    Update delete-deployment-preview.sh

--- scripts/delete-deployment-preview.sh
+++ scripts/delete-deployment-preview.sh
@@ -14,28 +14,13 @@ echo "Filtered deployments ${uid}"
 echo "action $ACTIONS to destroy"
 echo "================================================================="
 
-if [ $ACTIONS == "plan" ]; then       
-    echo $deployments | jq -r --arg branch "$BRANCH" '.deployments[] | select(.meta.githubCommitRef == $branch)'
-    for id in $uid; do
-        echo "Plan to delete deployment "
-        echo $id
-        echo "================================================================="       
-    done
-elif [ $ACTIONS == "apply" ]; then
-    for id in $uid; do
-        echo "================================================================="
-        echo "Deleting deployment $id ..."        
-        delete_url="${DELETE_DEPLOYMENTS_ENDPOINT}/${id}?teamId=${VERCEL_ORG_ID}"
-        response=$(curl -s -X DELETE "$delete_url" -H "Authorization: Bearer $VERCEL_TOKEN")
-        if [[ "$response" -eq 200 || "$response" -eq 204 ]]; then
-            echo "Deployment $id successfully deleted!"
-        else
-            echo "Failed to delete deployment $id. Response code: $response"
-            exit 1
-        fi
-    done
-else
-    echo "Invalid action specified. Please use 'plan' or 'apply'."
-    echo "Usage: $0 <branch> <action>"
-    
-fi
+
+for id in $uid; do
+    echo "================================================================="
+    echo "Deleting deployment $id ..."        
+    delete_url="${DELETE_DEPLOYMENTS_ENDPOINT}/${id}?teamId=${VERCEL_ORG_ID}"
+    response=$(curl -s -X DELETE "$delete_url" -H "Authorization: Bearer $VERCEL_TOKEN")
+
+
+done
+
