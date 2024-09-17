8dd3d8785683daca3a8e956a79beb668b75b1c66
Vadym Trufyn <148262369+VadimTrufyn@users.noreply.github.com>
2024-09-17 15:00:14 +0300

    Update delete-deployment-preview.sh

--- scripts/delete-deployment-preview.sh
+++ scripts/delete-deployment-preview.sh
@@ -11,7 +11,7 @@ ACTIONS=$2
 deployments=$(curl -s -X GET "$GET_DEPLOYMENTS_ENDPOINT/?projectId=$VERCEL_PROJECT_ID&teamId=$VERCEL_ORG_ID&limit=1000" -H "Authorization: Bearer $VERCEL_TOKEN ")
 
 filtered_deployments=$(echo $(curl -s -X GET "$GET_DEPLOYMENTS_ENDPOINT/?projectId=$VERCEL_PROJECT_ID&teamId=$VERCEL_ORG_ID&limit=1000" -H "Authorization: Bearer $VERCEL_TOKEN ") | jq -r --arg branch "$BRANCH" '.deployments[] | select(.meta.githubCommitRef == $branch) | .uid' )
-uid="${filtered_deployments//\"/}" # Remove double quotes
+uid="${filtered_deployments//\"/}"  # Remove double quotes
 echo "Filtered deployments ${uid}"
 echo "action $ACTIONS to destroy"
 echo "================================================================="
