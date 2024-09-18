8e96c54d192da6403627484938c7ede6416a6eaa
Vadym Trufyn <148262369+VadimTrufyn@users.noreply.github.com>
2024-09-18 15:38:06 +0300

    Update delete-deployment-preview.sh

--- scripts/delete-deployment-preview.sh
+++ scripts/delete-deployment-preview.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
+
+VERCEL_PROJECT_ID=prj_GF2SVVkgkr9fTe2UAbwMn3nPogMh
+VERCEL_ORG_ID=team_jjtPNd6UnSqoqdfHQr2oNAnG
+VERCEL_TOKEN=NlGa3rGGAbnRunGJIqNR6sVp
 # Get the Vercel API endpoints.
 GET_DEPLOYMENTS_ENDPOINT="https://api.vercel.com/v6/deployments"
 DELETE_DEPLOYMENTS_ENDPOINT="https://api.vercel.com/v13/deployments"
 
-# Create a list of deployments.
+ACTIONS=apply
 
-echo "================================================================="
-filtered_deployments=$(curl  -s -X GET "$GET_DEPLOYMENTS_ENDPOINT/?projectId=$VERCEL_PROJECT_ID&teamId=$VERCEL_ORG_ID&limit=1000" -H "Authorization: Bearer $VERCEL_TOKEN" | jq -r --arg branch "$BRANCH" '.deployments[] | select(.meta.githubCommitRef == $branch) | .uid')
-echo $filtered_deployments
-uid="${filtered_deployments//\"/}"
+# Create a list of deployments.
+deployments=$(curl -s -X GET "$GET_DEPLOYMENTS_ENDPOINT/?projectId=$VERCEL_PROJECT_ID&teamId=$VERCEL_ORG_ID&limit=1000" -H "Authorization: Bearer $VERCEL_TOKEN ")
+
+filtered_deployments=$(echo $deployments | jq -r --arg branch "$BRANCH" '.deployments[] | select(.meta.githubCommitRef == $branch) | .uid' )
+uid="${filtered_deployments//\"/}" # Remove double quotes
 echo "Filtered deployments ${uid}"
 echo "action $ACTIONS to destroy"
 echo "================================================================="
 
+if [ $ACTIONS == "plan" ]; then       
+    echo $deployments | jq -r --arg branch "$BRANCH" '.deployments[] | select(.meta.githubCommitRef == $branch)'
+    for id in $uid; do
+        echo "Plan to delete deployment "
+        echo $id
+        echo "================================================================="       
+    done
+elif [ $ACTIONS == "apply" ]; then
+    for id in $uid; do
+        echo "================================================================="
+        echo "Deleting deployment $id ..."        
+        delete_url="${DELETE_DEPLOYMENTS_ENDPOINT}/${id}?teamId=${VERCEL_ORG_ID}"
+        response=$(curl -s -X DELETE "$delete_url" -H "Authorization: Bearer $VERCEL_TOKEN")
 
-for id in $uid; do
-    echo "================================================================="
-    echo "Deleting deployment $id ..."        
-    delete_url="${DELETE_DEPLOYMENTS_ENDPOINT}/${id}?teamId=${VERCEL_ORG_ID}"
-    curl -s -X DELETE "$delete_url" -H "Authorization: Bearer $VERCEL_TOKEN"
-done
-
-
-
+    done
+else
+    echo "Invalid action specified. Please use 'plan' or 'apply'."
+    echo "Usage: $0 <branch> <action>"
+    
+fi
