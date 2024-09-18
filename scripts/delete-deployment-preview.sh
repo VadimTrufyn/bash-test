d72d0af9fc3163359bd4fe367dc597838cf05495
Vadym Trufyn <148262369+VadimTrufyn@users.noreply.github.com>
2024-09-18 15:22:31 +0300

    Update delete-deployment-preview.sh

--- scripts/delete-deployment-preview.sh
+++ scripts/delete-deployment-preview.sh
@@ -6,7 +6,8 @@ DELETE_DEPLOYMENTS_ENDPOINT="https://api.vercel.com/v13/deployments"
 
 # Create a list of deployments.
 deployments=$(curl -s -X GET "$GET_DEPLOYMENTS_ENDPOINT/?projectId=$VERCEL_PROJECT_ID&teamId=$VERCEL_ORG_ID&limit=1000" -H "Authorization: Bearer $VERCEL_TOKEN ")
-
+echo $deployments
+echo "================================================================="
 filtered_deployments=$(curl -s -X GET "$GET_DEPLOYMENTS_ENDPOINT/?projectId=$VERCEL_PROJECT_ID&teamId=$VERCEL_ORG_ID&limit=1000" \
 -H "Authorization: Bearer $VERCEL_TOKEN" \
 | jq -r --arg branch "$BRANCH" '.deployments[] | select(.meta.githubCommitRef == $branch) | .uid')
