eadeee3b3acc27ee0a76ad5d1e8e9bcf77a39e17
Vadym Trufyn <148262369+VadimTrufyn@users.noreply.github.com>
2024-09-18 15:18:02 +0300

    Update delete-deployment-preview.sh

--- scripts/delete-deployment-preview.sh
+++ scripts/delete-deployment-preview.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-
 # Get the Vercel API endpoints.
 GET_DEPLOYMENTS_ENDPOINT="https://api.vercel.com/v6/deployments"
 DELETE_DEPLOYMENTS_ENDPOINT="https://api.vercel.com/v13/deployments"
 
-ACTIONS=$1
-
 # Create a list of deployments.
-filtered_deployments==$(curl -s -X GET "$GET_DEPLOYMENTS_ENDPOINT/?projectId=$VERCEL_PROJECT_ID&teamId=$VERCEL_ORG_ID&limit=1000" -H "Authorization: Bearer $VERCEL_TOKEN " | jq -r --arg branch "$BRANCH" '.deployments[] | select(.meta.githubCommitRef == $branch) | .uid')
-uid=${filtered_deployments//\"/}
+deployments=$(curl -s -X GET "$GET_DEPLOYMENTS_ENDPOINT/?projectId=$VERCEL_PROJECT_ID&teamId=$VERCEL_ORG_ID&limit=1000" -H "Authorization: Bearer $VERCEL_TOKEN ")
+
+filtered_deployments=$(curl -s -X GET "$GET_DEPLOYMENTS_ENDPOINT/?projectId=$VERCEL_PROJECT_ID&teamId=$VERCEL_ORG_ID&limit=1000" \
+-H "Authorization: Bearer $VERCEL_TOKEN" \
+| jq -r --arg branch "$BRANCH" '.deployments[] | select(.meta.githubCommitRef == $branch) | .uid')
+
+uid="${filtered_deployments//\"/}"
 echo "Filtered deployments ${uid}"
 echo "action $ACTIONS to destroy"
 echo "================================================================="
@@ -19,9 +21,8 @@ for id in $uid; do
     echo "================================================================="
     echo "Deleting deployment $id ..."        
     delete_url="${DELETE_DEPLOYMENTS_ENDPOINT}/${id}?teamId=${VERCEL_ORG_ID}"
-    response=$(curl -s -X DELETE "$delete_url" -H "Authorization: Bearer $VERCEL_TOKEN")
-    echo $response
-    sleep 8
-
+    curl -s -X DELETE "$delete_url" -H "Authorization: Bearer $VERCEL_TOKEN"
 done
+
+
 
